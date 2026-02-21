4c70dd3069ffacd4b7d30b44fcd937af66f9562e
aziz0810 <mohamedazizzouari2@gmail.com>
2026-02-21 22:32:13 +0100

    "env"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/gestion-app-api'
+  apiUrl: 'http://localhost:1234/gestion-app-api',
+  firebase: {
+    apiKey: "AIzaSyBJX8MxP_aGla3vDz2eXaPLjFPEoCtNfwc",
+    authDomain: "gestion-app-c50c4.firebaseapp.com",
+    projectId: "gestion-app-c50c4",
+    storageBucket: "gestion-app-c50c4.firebasestorage.app",
+    messagingSenderId: "976796519201",
+    appId: "1:976796519201:web:25bad69243e7584eb70c60",
+    measurementId: "G-PLY3SN7BXK"
+  }
 };
